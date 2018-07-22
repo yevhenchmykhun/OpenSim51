60299e32d465b8ab25d628308b97e7a68838142f
Yevhen Chmykhun <evgenii.chmyhun@gmail.com>
2018-07-22 23:56:42 +0300

    Implement translate method in Simulator class

--- src/main/java/simulator/Simulator.java
+++ src/main/java/simulator/Simulator.java
@@ -1,6 +1,14 @@
 package simulator;
 
+import assembler.syntaxanalyzer.Asm8051Lexer;
+import assembler.syntaxanalyzer.Asm8051Parser;
+import assembler.syntaxanalyzer.Asm8051PassOneVisitor;
+import assembler.syntaxanalyzer.Asm8051PassTwoVisitor;
 import intelhexparser.Intel8HexParser;
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
 import simulator.instruction.Command;
 import simulator.instruction.Instruction;
 import simulator.memory.ExternalCode;
@@ -11,12 +19,25 @@ import simulator.memory.datatype.UnsignedInt8;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.util.HashMap;
 
 public class Simulator {
 
     private final InternalData internalData = new InternalData();
     private final ExternalData externalData = new ExternalData();
     private final ExternalCode externalCode = new ExternalCode();
+
+    public InternalData getInternalData() {
+        return internalData;
+    }
+
+    public ExternalData getExternalData() {
+        return externalData;
+    }
+
+    public ExternalCode getExternalCode() {
+        return externalCode;
+    }
 
     private UnsignedInt16 programCounter = new UnsignedInt16(0);
 
@@ -36,6 +57,21 @@ public class Simulator {
             }
         });
         hexParser.parse();
+    }
+
+    public void translate(String sourceCode) {
+        CharStream stream = CharStreams.fromString(sourceCode);
+        Asm8051Lexer lexer = new Asm8051Lexer(stream);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        Asm8051Parser parser = new Asm8051Parser(tokens);
+        ParseTree tree = parser.source();
+
+        HashMap<String, UnsignedInt16> symbolTable = new HashMap<>();
+        Asm8051PassOneVisitor passOneVisitor = new Asm8051PassOneVisitor(symbolTable);
+        passOneVisitor.visit(tree);
+
+        Asm8051PassTwoVisitor passTwoVisitor = new Asm8051PassTwoVisitor(externalCode, symbolTable);
+        passTwoVisitor.visit(tree);
     }
 
     public void run() {
